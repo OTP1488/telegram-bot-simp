a66e04ce25f5377de6f446b3a30c26b204a091e5
OTP1488 <qwerty360001@gmail.com>
2026-06-23 17:57:49 +0300

    Update telobal.py

--- telobal.py
+++ telobal.py
@@ -4,11 +4,11 @@ import json
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://postgres:voQbMdZyzvLkQuFobYhYLPYSEJtrQvjr@postgres.railway.internal:5432/railway")
 
 
 def get_conn():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(postgresql://postgres:voQbMdZyzvLkQuFobYhYLPYSEJtrQvjr@postgres.railway.internal:5432/railway)
 
 
 def init_db():

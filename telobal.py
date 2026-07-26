d51e17f821809b4808f19a24eb101a87cb6fb9ab
OTP1488 <qwerty360001@gmail.com>
2026-07-26 12:11:00 +0300

    Refactor telobal.py for improved readability

--- telobal.py
+++ telobal.py
@@ -71,14 +71,14 @@ JOKER_NUMBERS = [
 ]
 
 MARTINEZ_NUMBERS = [
-    normalize("380947123505"),
+    normalize("0"),
     normalize("0"),
     normalize("0"),
 
 ]
 
 KKAZANTSEVV_NUMBERS = [
-    normalize("0"),
+    normalize("380947123505"),
     normalize("0"),
     normalize("0"),
 

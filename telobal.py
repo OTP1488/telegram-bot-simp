48e684287db2cc18f0dfe525f2808df020bc0fd3
OTP1488 <qwerty360001@gmail.com>
2026-07-26 16:49:20 +0300

    Update telobal.py

--- telobal.py
+++ telobal.py
@@ -60,7 +60,7 @@ ZPD_NUMBERS = [
 
 BROKEN_NUMBERS = [
     normalize("380947100369"),
-    normalize("380947100640"),
+    normalize("0"),
     normalize("380947100775"),
 ]
 
@@ -71,7 +71,7 @@ JOKER_NUMBERS = [
 ]
 
 MARTINEZ_NUMBERS = [
-    normalize("0"),
+    normalize("380947100640"),
     normalize("0"),
     normalize("0"),
 
